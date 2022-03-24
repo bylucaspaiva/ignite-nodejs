181457eff01f92a4803b47ab5b3ac662a7e3fd63
bylucaspaiva <lucasp4iva@gmail.com>
2022-03-24 13:55:29 -0300

    feat: adding middleware

--- FinAPI/src/index.js
+++ FinAPI/src/index.js
@@ -7,6 +7,22 @@ app.use(express.json());
 
 const customers = [];
 
+//Middleware
+function verifyIfExistsAccountCPF(request, response, next) {
+  const {cpf} = request.headers;
+
+  //find return the object while some return boolean
+  const customer = customers.find(customer => customer.cpf === cpf);
+
+  if(!customer) {
+    return response.status(400).json({error: "Customer not found!"})
+  }
+
+  request.customer = customer;
+
+  return next();
+}
+
 app.post("/account", (request, response) => {
   const { cpf, name } = request.body; 
 
@@ -28,12 +44,9 @@ app.post("/account", (request, response) => {
   return response.status(201).send()
 })
 
-app.get("/statement/:cpf", (request, response) => {
-  const {cpf} = request.params;
-
-  //find return the object while some return boolean
-  const customer = customers.find(customer => customer.cpf === cpf);
-
+app.get("/statement", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+  
   return response.json(customer.statement);
 })
 
